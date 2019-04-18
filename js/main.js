24e09237f5954aaf07762fcc2be0d2b6a8d53740
shojol <dip_shojol@yahoo.com>
2019-04-18 14:10:54 -0400

    carousel scrolling issue fixed

--- js/main.js
+++ js/main.js
@@ -2,43 +2,45 @@
 
 jQuery(document).ready(() => {
 
-	let counter = 0;
+    let counter = 0;
 
-	jQuery('.first-arrow').on("click", ()=>{
-if (counter <= 0){
-    	jQuery('.suggestions ul').css({
-    		'transition': 'transform 0.5s ease-in-out',
-    		'transform': `translateX(${counter}px)`
-    		})
-
-    	counter += 100;
-
-    	console.log(counter);
+    jQuery('.first-arrow').on("click", () => {
+        if (counter <= 0) {
+            counter += 100;
+            jQuery('.suggestions ul').css({
+                'transition': 'transform 0.5s ease-in-out',
+                'transform': `translateX(${counter}px)`
+            })
 
 
-}
-    	// jQuery('.suggestions ul').style.transition = 'transform 0.5s ease-in-out';
-    	// counter++;
-    	// jQuery('.suggestions ul').style.transform = 'translateX(30px)';
+
+            console.log(counter);
+
+
+        }
+        // jQuery('.suggestions ul').style.transition = 'transform 0.5s ease-in-out';
+        // counter++;
+        // jQuery('.suggestions ul').style.transform = 'translateX(30px)';
     });
 
 
-    jQuery('.last-arrow').on("click", ()=>{
+    jQuery('.last-arrow').on("click", () => {
 
-    	if (counter >= -850) {
-    	jQuery('.suggestions ul').css({
-    		'transition': 'transform 0.5s ease-in-out',
-    		'transform': `translateX(${counter}px)`
-    		})
+        if (counter >= -850) {
+            counter += -100;
+            jQuery('.suggestions ul').css({
+                'transition': 'transform 0.5s ease-in-out',
+                'transform': `translateX(${counter}px)`
+            })
 
-    	counter += -100;
 
-    	console.log(counter);
 
-}
+            console.log(counter);
 
-    	// jQuery('.suggestions ul').style.transition = 'transform 0.5s ease-in-out';
-    	// counter++;
-    	// jQuery('.suggestions ul').style.transform = 'translateX(30px)';
+        }
+
+        // jQuery('.suggestions ul').style.transition = 'transform 0.5s ease-in-out';
+        // counter++;
+        // jQuery('.suggestions ul').style.transform = 'translateX(30px)';
     });
 });
